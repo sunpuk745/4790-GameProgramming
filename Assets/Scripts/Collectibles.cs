d88a896732de261ccbf32e9a1bb1af18bcf3afe6
SoulLeaf <kittipat.sa@kkumail.com>
2022-09-18 21:09:26 +0700

    Finished respawnable collectible, and spring

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -6,34 +6,46 @@ public class Collectibles : MonoBehaviour
 {
     [SerializeField] private SOCollectibles collectibleObject;
 
-    CollectibleType _collectibletype = CollectibleType.Random;
-
-    private Transform player;
     private int randomNum;
+    [SerializeField]private float respawnTimer = 4f;
+
+    private const string PlayerTag = "Player";
 
     public SpriteRenderer collectableSprite;
     public SpriteRenderer playerSprite;
+    private Collider2D collectibleCollider2D;
     
     [SerializeField] private Sprite[] sprites;
     
     private void Start() 
     {
+        collectibleObject.GetCollectibleType();
+        collectibleObject.GetRespawnable();
+        collectibleObject.GetSprite();
+        collectibleObject.GetOutlineSprite();
         //Debug.Log(collectibleObject.GetCollectibleType());
         //Debug.Log(collectibleObject.GetCollectible());
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        playerSprite = player.GetComponent<SpriteRenderer>();
+        playerSprite = GameObject.Find("Model").GetComponent<SpriteRenderer>();
         collectableSprite = GetComponent<SpriteRenderer>();
-        if (gameObject.activeSelf)
+        collectibleCollider2D = GetComponent<Collider2D>();
+
+        if (gameObject.activeSelf && collectibleObject.GetCollectibleType() == CollectibleType.Random)
         {
             StartCoroutine(RandomCrystal());
         }
     }
 
-    IEnumerator RandomCrystal()
+    public void EnableCollectible()
+    {
+        collectibleCollider2D.enabled = true;
+        collectableSprite.sprite = collectibleObject.GetSprite();
+    }
+
+    private IEnumerator RandomCrystal()
     {
         collectableSprite.sprite = sprites[0];
         yield return new WaitForSeconds(1f);
-        if (_collectibletype == CollectibleType.Random)
+        if (collectibleObject.GetCollectibleType() == CollectibleType.Random)
         {
             randomNum = Random.Range (1, sprites.Length);
             //Debug.Log(randomNum);
@@ -44,39 +56,61 @@ public class Collectibles : MonoBehaviour
         StartCoroutine(RandomCrystal());
     }
 
+    private IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(respawnTimer);
+        EnableCollectible();
+    }
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        switch (collectibleObject.GetCollectibleType())
+        if (other.CompareTag(PlayerTag))
         {
-            case CollectibleType.Red:
-            playerSprite.color = Color.red;
-            //Debug.Log("Picked up Red Crystal!");
-            break;
-            case CollectibleType.Green:
-            playerSprite.color = Color.green;
-            //Debug.Log("Picked up Green Crystal!");
-            break;
-            case CollectibleType.Blue:
-            playerSprite.color = Color.blue;
-            //Debug.Log("Picked up Blue Crystal!");
-            break;
-            case CollectibleType.Random:
-            if (randomNum == 1)
+            switch (collectibleObject.GetCollectibleType())
             {
+                case CollectibleType.Red:
                 playerSprite.color = Color.red;
-            }
-            else if (randomNum == 2)
-            {
+                //Debug.Log("Picked up Red Crystal!");
+                break;
+                case CollectibleType.Green:
                 playerSprite.color = Color.green;
-            }
-            else if (randomNum == 3)
-            {
+                //Debug.Log("Picked up Green Crystal!");
+                break;
+                case CollectibleType.Blue:
                 playerSprite.color = Color.blue;
+                //Debug.Log("Picked up Blue Crystal!");
+                break;
+                case CollectibleType.Random:
+                if (randomNum == 1)
+                {
+                    playerSprite.color = Color.red;
+                }
+                else if (randomNum == 2)
+                {
+                    playerSprite.color = Color.green;
+                }
+                else if (randomNum == 3)
+                {
+                    playerSprite.color = Color.blue;
+                }
+                break;
+                case CollectibleType.DoubleJump:
+                collectibleCollider2D.enabled = false;
+                break;
+                default:
+                break;
             }
-            break;
-            default:
-            break;
+            if (!collectibleObject.GetRespawnable())
+            {
+                //Debug.Log("Picked up Unrespawnable!");
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                //Debug.Log("Picked up Respawnable!");
+                collectableSprite.sprite = collectibleObject.GetOutlineSprite();
+                StartCoroutine(Respawn());
+            }
         }
-        Destroy(gameObject);
     }
 }
