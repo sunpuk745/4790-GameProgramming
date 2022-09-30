a6fb37274ef09dbce8f359e106576524e7dce2e7
SoulLeaf <kittipat.sa@kkumail.com>
2022-09-30 23:06:16 +0700

    kill dotween

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -72,6 +72,7 @@ public class GameManager : MonoBehaviour
     public void RestartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        DOTween.KillAll();
     }
 
     public void NextLevel()
